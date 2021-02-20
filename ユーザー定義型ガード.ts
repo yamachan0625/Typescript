73c867ed0ac71859019d041fb963c60a8c791458
yamachan0625 <napoli.080625@gmail.com>
2021-02-21 01:06:45 +0900

    Update ユーザー定義型ガード.ts

--- ユーザー定義型ガード.ts
+++ ユーザー定義型ガード.ts
@@ -4,12 +4,20 @@ function isString(a: unknown): boolean {
 function parseInput(input: string | number) {
   let formattedInput: string;
   if (isString(input)) {
+    // ここでエラー
+    // typoeofによる型の絞り込みはスコープを離れると機能しない
+    formattedInput = input.toUpperCase();
   }
-  // ここでエラー
-  // typoeofによる型の絞り込みはスコープを離れると機能しない
-  formattedInput = input.toUpperCase();
 }
 // booleanを返す関数の場合ユーザー定義型ガードを使用するとその関数を使う時に絞り込みが引き継がれる
 function isString2(a: unknown): a is string {
   return typeof a === 'string';
+}
+
+function parseInput2(input: string | number) {
+  let formattedInput: string;
+  if (isString2(input)) {
+    // 別スコープでも機能する
+    formattedInput = input.toUpperCase();
+  }
 }
